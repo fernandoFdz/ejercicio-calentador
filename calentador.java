6a31ea3bac4c704f9b1355e843ca0b6adf364be0
fernandoFdz <ferykudouxd@hotmail.com>
2017-09-01 00:17:44 -0500

    se han corregido los errores

--- calentador.java
+++ calentador.java
@@ -1,32 +1,56 @@
+/*
+ * clase de tipo calentador
+ */
 public class calentador{
+    /*
+     * variables de instancia 
+     */
     private int temperatura;
-    public int incremento;
+    private int incremento;
     private int min;
     private int max;
-        public void estandar(){
+    
+        /*
+         * constructor: inicializa las variables a utilizar
+         */
+        
+        public calentador(){
             temperatura=10;
             incremento=3;
             min=0;
             max=40;
         }
         
-        public int sube(){
-            if(temperatura<max){             
+        /*
+         * metodo que aumenta la temperatura
+         */
+            public int sube(){
+                if(temperatura<max){             
                     temperatura = temperatura + incremento;
             }
             return temperatura;
         } 
         
+        /*
+         *  metodo que dismiye la temperatura
+         */
         public int baja(){
             if (temperatura < min){
             temperatura = temperatura - incremento;
             }return temperatura;
         }
         
-        public int muestra(){
+        /*
+         * metodo que retorna la variable
+         */
+        public int muestra(){ 
             return temperatura;
         }
         
+        
+        /* 
+         * aqui se aplica la encapsulacion ya que solo manda a llamar ciertos metodos
+         */
         public void modificarIncremento(){
             
             if(incremento <0){
